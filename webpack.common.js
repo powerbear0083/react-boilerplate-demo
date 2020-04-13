334dec996ed57b62c84df17e372cb048c725519b
yinhsun <powerbear0083@gmail.com>
2020-04-13 08:16:29 +0800

    webpack 基本分拆檔案

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,13 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
 const path = require('path')
+
 
 
 module.exports = {
     entry: {
         main: './src/index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: ['/node_modules'],
-                use: ['babel-loader']
-            },
-            {
-                test: /\.s(a|c)ss$/,
-                use: [ 
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                          plugins: [
-                            require('autoprefixer')
-                          ]
-                        }
-                    },
-                    'sass-loader'
-                ]
-            }
-        ]
     },
     resolve: {
         extensions: ['*', '.js', '.jsx', '.sass', '.scss']
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,19 +2,60 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const webpack = require('webpack')
 
-module.exports = merge( common, 
-    {
-        mode: 'development',
-        // devtool: 'source-map',
-        devtool: 'eval-source-map',
-        plugins: [
-            new webpack.HotModuleReplacementPlugin()
-        ],
-        devServer: {
-            stats: 'errors-only',
-            host: process.env.HOST,
-            port: 7788,
-            open: true
+module.exports = merge(
+    [
+        common,
+        {
+            mode: 'development',
+            module: {
+                rules: [
+                    {
+                        test: /\.(js|jsx)$/,
+                        exclude: ['/node_modules'],
+                        use: ['babel-loader']
+                    },
+                    {
+                        test: /\.s(a|c)ss$/,
+                        use: [
+                            'style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true,
+                                }
+                            },
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    plugins: [
+                                        require('autoprefixer')
+                                    ]
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true,
+                                    sassOptions: {
+                                        outputStyle: 'compressed',
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            // devtool: 'source-map',
+            devtool: 'eval-source-map',
+            plugins: [
+                new webpack.HotModuleReplacementPlugin()
+            ],
+            devServer: {
+                stats: 'errors-only',
+                host: process.env.HOST,
+                port: 7788,
+                open: true
+            }
         }
-    }
+    ]
 )
